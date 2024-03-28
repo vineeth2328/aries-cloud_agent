f24838298e2d861d9d15d21dab623e5a1475a8b2
vineeth2328 <vineethboppudi23@gmail.com>
2024-03-28 12:55:08 +0530

    added middleware

---
+++ identity-middleware/config.js
@@ -0,0 +1,9 @@
+// config.js
+const dotenv = require('dotenv');
+dotenv.config();
+let config = {
+    INDY_WEBSERVER_URL: "http://localhost:9000",
+    ISSUER_AGENT_API_URL: "http://localhost:11000",
+    HOLDER_AGENT_API_URL: "http://localhost:11001"
+};
+module.exports=config
